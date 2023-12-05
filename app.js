cf983508c3e62ed1fbb5b2bd8a0348dc80bca256
Brice Cabioch <cabioch.brice@gmail.com>
2023-12-05 21:13:27 +0100

    lab completed

--- app.js
+++ app.js
@@ -27,6 +27,52 @@ spotifyApi
   );
 
 // Our routes go here:
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/artist-search", (req, res) => {
+  const { artistName } = req.query;
+  spotifyApi
+    .searchArtists(artistName)
+    .then((data) => {
+      console.log(
+        "The received data from the API: ",
+        data.body.artists.items[0]
+      );
+      const artists = data.body.artists.items;
+      res.render("artist-search-results", { artists });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+app.get("/albums/:artistId", (req, res, next) => {
+  const artistId = req.params.artistId;
+
+  spotifyApi
+    .getArtistAlbums(artistId)
+    .then((data) => {
+      console.log("Artist albums", data.body);
+      const albums = data.body.items;
+      res.render("albums", { albums });
+    })
+    .catch((err) => console.log("Error while getting artist albums: ", err));
+});
+
+app.get("/tracks/:albumId", (req, res, next) => {
+  const albumId = req.params.albumId;
+
+  spotifyApi
+    .getAlbumTracks(albumId)
+    .then((data) => {
+      console.log("Album tracks", data.body);
+      const tracks = data.body.items;
+      res.render("tracks", { tracks });
+    })
+    .catch((err) => console.log("Error while getting album tracks: ", err));
+});
 
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
